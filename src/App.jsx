441cc8fc41effed9858b2c5824241723348a5ad7
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-28 12:21:01 +0100

    Update webpage title and enhance styling; add global CSS reset and variables; implement footer with social links

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-import ModeToggle from "./components/ModeToggle"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import ModeToggle from "./components/ModeToggle";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <ModeToggle/>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer/>
+      <div lassName={styles.app}>
+        <header>
+          <ModeToggle />
+          <Navbar />
+        </header>
+        <main className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+          <Footer />
+        </main>
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/global.css";
 
